d31c2f9f1bb85e562c359b98da02d7ababa254e8
Queren Hapuque <queren.fernandes@yahoo.com>
2022-07-03 11:13:09 -0300

    Add files via upload

---
+++ String_Concatenacao.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() 
+{
+	char str_1[10] = {"Teste"};
+	char str_2[10] = {"Queren"};
+	
+	strcat(str_1, str_2);
+	printf("%s", str_1);
+}
